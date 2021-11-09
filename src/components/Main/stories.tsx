4700ae27b1ff1ca04d0b2547c8705e38a0acb4bc
Antonio Marcos <marcos-12marcos@hotmail.com>
2021-11-09 20:27:49 -0300

    chore: test

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -12,7 +12,7 @@ export default {
 
 export const Basic: Story = (args) => <Main {...args} />
 Basic.args = {
-  title: 'title bassic',
+  title: 'title basic',
   description: 'description basic'
 }
 
