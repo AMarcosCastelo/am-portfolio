54000b5d5b479a8673b920bd77721af54e07eae3
Antonio Marcos <marcos-12marcos@hotmail.com>
2021-11-09 20:24:54 -0300

    fe(sadada): test 6

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -12,7 +12,7 @@ export default {
 
 export const Basic: Story = (args) => <Main {...args} />
 Basic.args = {
-  title: 'title basssic',
+  title: 'title bassic',
   description: 'description basic'
 }
 
