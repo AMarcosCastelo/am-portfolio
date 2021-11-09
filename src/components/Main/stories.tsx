f61303bf14a8fec68296495320ae50b7360ca4a1
Antonio Marcos <marcos-12marcos@hotmail.com>
2021-11-09 20:08:04 -0300

    chore: test commit lint 2

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -12,7 +12,7 @@ export default {
 
 export const Basic: Story = (args) => <Main {...args} />
 Basic.args = {
-  title: 'title basic',
+  title: 'title baaasic',
   description: 'description basic'
 }
 
