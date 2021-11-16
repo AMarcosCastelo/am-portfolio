935e731582e81d2337fb0f7acf81ba4791461133
Antonio Marcos <marcos-12marcos@hotmail.com>
2021-11-15 21:07:32 -0300

    feat(comp): power button

--- src/components/PowerButton/index.tsx
+++ src/components/PowerButton/index.tsx
@@ -1,10 +1,13 @@
+import NextLink from 'next/link'
 import { PowerBtn } from 'components/AllSvgs'
 
 import * as S from './styles'
 
 const PowerButton = () => (
   <S.Power>
-    <PowerBtn width={30} height={30} fill="currentColor" />
+    <NextLink href="/">
+      <PowerBtn width={30} height={30} fill="currentColor" />
+    </NextLink>
   </S.Power>
 )
 
